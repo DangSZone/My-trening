fcaa22d7f756b7a7cce90434204c579aa68e43b5
DangSZone <googlolochka@gmail.com>
2024-10-07 18:36:35 +0700

    Add files via upload

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,3 +1,5 @@
+
+
 data_structure = [
 [1, 2, 3],
 {'a': 4, 'b': 5},
@@ -5,33 +7,27 @@ data_structure = [
 "Hello",
 ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-
-
+sum_ = 0
 def calculate_structure_sum(*i):
-    first = i[0]
-    sum_ = 0
-    if type(first) == list:
-        for n in first:
-            sum_ += int(n)
-    elif type(first) == dict:
-        for key, value in first.items():
-            sum_ += len(key) + int(value)
-    elif type(first) == tuple:
-        for l in first:
-            if type(l) == int:
-                sum_ += l
-            elif type(l) == dict:
-                for key, value in l.items():
-                    sum_ += len(key) + int(value)
-    elif type(first) == str:
-        sum_ += len(first)
-        pass
-    print(first)
-    return sum_ + calculate_structure_sum(*i[1:])
+    global sum_
+    for el in i:
+        if isinstance(el,(list,tuple)):
+           calculate_structure_sum(*el)
+        elif isinstance(el, dict):
+            for k in el.items():
+                calculate_structure_sum(k)
+        elif isinstance(el, int):
+            sum_ += el
+        elif isinstance(el, str):
+            sum_ += (len(el))
+        else:
+            calculate_structure_sum(*el)
+    return sum_
 
 
-result = calculate_structure_sum(*data_structure)
-print(result)
+
+print(calculate_structure_sum(data_structure))
+
 
 
 
