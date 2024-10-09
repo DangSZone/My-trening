5672ca3970d6ca09bcb7e569aef69e389cd3bbdc
DangSZone <googlolochka@gmail.com>
2024-10-09 22:34:06 +0700

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,11 @@
+def divide(first,second):
+	if second == 0:
+		return 'Ошибка'
+	else:
+		return round((first / second),2)
+
+
+
+
+
+
---
+++ module_4_1.py
@@ -0,0 +1,16 @@
+from fake_math import divide as fd
+from true_math import divide as td
+
+res = fd(381,9)
+print(res)
+
+res2 = fd(3,0)
+print(res2)
+
+res3 = td(456,87)
+print(res3)
+
+res4 = td(78,0)
+print(res4)
+
+
