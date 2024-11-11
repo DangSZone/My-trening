e916bd6a1f8539ed35371acec7dc1fd0e4aa3d98
DangSZone <googlolochka@gmail.com>
2024-11-11 14:10:16 +0700

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -1,23 +1,14 @@
-strings_positions = {}
-def custom_write(file_name, strings):
 
+def custom_write(file_name, strings):
+    strings_positions = {}
     line_number = 1
-    file = open(file_name, encoding='Utf-8')
-    file_lines = file.readlines()
+    file_wr = open(file_name, 'w+', encoding='Utf-8')
     for s in strings:
-        for f_s in file_lines:
-            if s in f_s:
-                break
-            elif s not in f_s:
-                continue
-        else:
-            file_wr = open(file_name, 'a', encoding='Utf-8')
             line_byte = file_wr.tell()
             file_wr.write(f'{s}\n')
             strings_positions[(line_number, line_byte)] = s
             line_number += 1
-            file_wr.close()
-    file.close()
+    file_wr.close()
     return strings_positions
 
 
